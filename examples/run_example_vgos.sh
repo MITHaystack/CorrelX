dc02896b5387e3a2c9dc9fa88fc74d84419500dd
ajvazquez <ajvazquez@users.noreply.github.com>
2017-03-01 15:56:16 -0500

    Update run_example_vgos.sh

--- examples/run_example_vgos.sh
+++ examples/run_example_vgos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Run: ./examples/run_example_vgos.sh
+# Run: source ./examples/run_example_vgos.sh
 # 
 python src/mapred_cx.py -c conf/correlx.ini -f exper=examples/test_dataset_vgos,serial=1,parallel=0
