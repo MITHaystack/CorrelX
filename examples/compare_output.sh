f0447388461c334ebddf589bf1ced4d9a7f7edf7
ajvazquez <ajvazquez@users.noreply.github.com>
2017-03-01 15:55:49 -0500

    Added source command

--- examples/compare_output.sh
+++ examples/compare_output.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Run: ./examples/compare_output.sh <path_to_output_file>
-# Example: ./examples/compare_output.sh examples/test_dataset_vgos/cx_20170228_144701/OUT_s0_v0.out
+# Run: source ./examples/compare_output.sh <path_to_output_file>
+# Example: source ./examples/compare_output.sh examples/test_dataset_vgos/cx_20170228_144701/OUT_s0_v0.out
 #
 python src/vis_compare.py examples/test_dataset_vgos/example_output/OUT_s0_v0.out $1
