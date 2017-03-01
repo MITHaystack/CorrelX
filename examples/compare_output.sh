75624f97d9ea8ab982adc5438baed3d9f55c311b
ajvazquez <ajvazquez@users.noreply.github.com>
2017-03-01 15:29:59 -0500

    Example dataset for VGOS (VLBI Global Observing System)

---
+++ examples/compare_output.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Replace paths if required
+# $1: path to output file (e.g. correlx/examples/test_dataset_vgos/cx_20170228_144701/OUT_s0_v0.out
+python correlx/src/vis_compare.py correlx/examples/test_dataset_vgos/example_output/OUT_s0_v0.out $1
